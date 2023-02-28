3d94eaa2175146eca8a3c5b5b328d69f668e56d8
A. P <alekspylypets@gmail.com>
2023-02-28 09:01:18 -0800

    Add solution

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-function areResponsesValid(questions, responses) {
-    // TODO: Implement this function
-    console.log("Questions:", questions);
-    console.log("Responses:", responses);
-    return false;
+function areResponsesValid (questions, responses) {
+  const responsesObj = {};
+  for (const resp of responses) {
+    responsesObj[resp.questionId] = resp.choice;
   }
-  
+
+  for (const {condition, id} of questions) {
+    const required = isRequired(condition, responsesObj);
+
+    if(required && !responsesObj[id] || !required && responsesObj[id]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function isRequired (condition, responses) {
+  if (!condition) return true;
+
+  if (condition.type === 'OR') {
+    return condition.subconditions.some((cond) => isRequired(cond, responses));
+  }
+
+  if (condition.type === 'AND') {
+    return condition.subconditions.every((cond) => isRequired(cond, responses));
+  }
+
+  return responses[condition.questionId] === condition.choice;
+}
